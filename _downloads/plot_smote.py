9ce45b059426b20ac54311f492b2870805a8c3e6
scikit-learn-contrib <g.lemaitre58@gmail.com>
2017-03-30 19:47:35 +0000

    Pushing the docs for revision for branch: master, commit 5ca30378f7d73522d7c1d38a05c3b9dc0c83d3bb

--- _downloads/plot_smote.py
+++ _downloads/plot_smote.py
@@ -1,14 +1,18 @@
 """
-=============
-SMOTE regular
-=============
+=====
+SMOTE
+=====
 
-An illustration of the random SMOTE regular method.
+An illustration of the SMOTE method and its variant.
 
 """
 
+# Authors: Fernando Nogueira
+#          Christos Aridas
+#          Guillaume Lemaitre <g.lemaitre58@gmail.com>
+# License: MIT
+
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.datasets import make_classification
 from sklearn.decomposition import PCA
 
@@ -16,18 +20,28 @@ from imblearn.over_sampling import SMOTE
 
 print(__doc__)
 
-sns.set()
 
-# Define some color for the plotting
-almost_black = '#262626'
-palette = sns.color_palette()
+def plot_resampling(ax, X, y, title):
+    c0 = ax.scatter(X[y == 0, 0], X[y == 0, 1], label="Class #0", alpha=0.5)
+    c1 = ax.scatter(X[y == 1, 0], X[y == 1, 1], label="Class #1", alpha=0.5)
+    ax.set_title(title)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+    ax.spines['left'].set_position(('outward', 10))
+    ax.spines['bottom'].set_position(('outward', 10))
+    ax.set_xlim([-6, 8])
+    ax.set_ylim([-6, 6])
+
+    return c0, c1
 
 
 # Generate the dataset
-X, y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
+X, y = make_classification(n_classes=2, class_sep=2, weights=[0.3, 0.7],
                            n_informative=3, n_redundant=1, flip_y=0,
                            n_features=20, n_clusters_per_class=1,
-                           n_samples=5000, random_state=10)
+                           n_samples=80, random_state=10)
 
 # Instanciate a PCA object for the sake of easy visualisation
 pca = PCA(n_components=2)
@@ -35,25 +49,29 @@ pca = PCA(n_components=2)
 X_vis = pca.fit_transform(X)
 
 # Apply regular SMOTE
-sm = SMOTE(kind='regular')
-X_resampled, y_resampled = sm.fit_sample(X, y)
-X_res_vis = pca.transform(X_resampled)
+kind = ['regular', 'borderline1', 'borderline2', 'svm']
+sm = [SMOTE(kind=k) for k in kind]
+X_resampled = []
+y_resampled = []
+X_res_vis = []
+for method in sm:
+    X_res, y_res = method.fit_sample(X, y)
+    X_resampled.append(X_res)
+    y_resampled.append(y_res)
+    X_res_vis.append(pca.transform(X_res))
 
 # Two subplots, unpack the axes array immediately
-f, (ax1, ax2) = plt.subplots(1, 2)
+f, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2)
+# Remove axis for second plot
+ax2.axis('off')
+ax_res = [ax3, ax4, ax5, ax6]
 
-ax1.scatter(X_vis[y == 0, 0], X_vis[y == 0, 1], label="Class #0", alpha=0.5,
-            edgecolor=almost_black, facecolor=palette[0], linewidth=0.15)
-ax1.scatter(X_vis[y == 1, 0], X_vis[y == 1, 1], label="Class #1", alpha=0.5,
-            edgecolor=almost_black, facecolor=palette[2], linewidth=0.15)
-ax1.set_title('Original set')
+c0, c1 = plot_resampling(ax1, X_vis, y, 'Original set')
+for i in range(len(kind)):
+    plot_resampling(ax_res[i], X_res_vis[i], y_resampled[i],
+                    'SMOTE {}'.format(kind[i]))
 
-ax2.scatter(X_res_vis[y_resampled == 0, 0], X_res_vis[y_resampled == 0, 1],
-            label="Class #0", alpha=.5, edgecolor=almost_black,
-            facecolor=palette[0], linewidth=0.15)
-ax2.scatter(X_res_vis[y_resampled == 1, 0], X_res_vis[y_resampled == 1, 1],
-            label="Class #1", alpha=.5, edgecolor=almost_black,
-            facecolor=palette[2], linewidth=0.15)
-ax2.set_title('SMOTE regular')
-
+ax2.legend((c0, c1), ('Class #0', 'Class #1'), loc='center',
+           ncol=1, labelspacing=0.)
+plt.tight_layout()
 plt.show()
